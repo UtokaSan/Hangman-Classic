554352ed28e27b3aa876d10b91b350bdc6182dd1
Florian Boulbes <florian.boulbes@ynov.com>
2022-10-05 18:36:11 +0200

    [OPTIMIZE] readFile function

--- main.go
+++ main.go
@@ -8,20 +8,16 @@ import (
 	"time"
 )
 
-func ChooseWord() {
+func readFile() {
 	data, _ := ioutil.ReadFile("words/words.txt")
 	file := string(data)
 	temp := strings.Split(file, "\n")
 	l := len(temp)
-}
-
-func main() {
-	data, _ := ioutil.ReadFile("words/words.txt")
-	file := string(data)
-	temp := strings.Split(file, "\n")
-	l := len(temp)
-	ChooseWord()
 	rand.Seed(time.Now().UnixNano())
 	j := rand.Intn(l)
 	fmt.Print(j)
+}
+
+func main() {
+	readFile
 }
