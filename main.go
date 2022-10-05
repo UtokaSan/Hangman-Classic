ac45b2a4762028d5a390f187ed723363a601a2f3
Florian Boulbes <florian.boulbes@ynov.com>
2022-10-05 18:43:53 +0200

    [UPDATE] Function readFile

--- main.go
+++ main.go
@@ -11,8 +11,13 @@ import (
 func readFile() {
 	data, _ := ioutil.ReadFile("words/words.txt")
 	file := string(data)
+	line := 0
 	temp := strings.Split(file, "\n")
 	l := len(temp)
+	for _,item := range temp {
+		fmt.Println(line,item)
+		line++
+	}
 	rand.Seed(time.Now().UnixNano())
 	j := rand.Intn(l)
 	fmt.Print(j)
