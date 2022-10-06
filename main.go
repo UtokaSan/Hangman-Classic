1c76fb83f9d477fe0fabb454de796ad1d6073dec
Florian Boulbes <florian.boulbes@ynov.com>
2022-10-06 19:57:38 +0200

    [UPDATE] function test for len work

--- main.go
+++ main.go
@@ -11,8 +11,10 @@ func Random(nbr int) int {
 	return rand.Intn(nbr)
 }
 func test() {
-	word := readFile("words/word.txt")
-	fmt.Print(word)
+	word := readFile("words/words.txt")
+	len := len(word)/2 - 1
+	str := string(word[len])
+	fmt.Print(str)
 }
 func readFile(fileAccess string) string {
 	data, _ := ioutil.ReadFile(fileAccess)
@@ -20,6 +22,7 @@ func readFile(fileAccess string) string {
 	split := strings.Split(str, "\n")
 	random := Random(len(split))
 	word := split[random]
+	fmt.Println(word)
 	return word
 }
 
