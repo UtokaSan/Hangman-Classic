55c004cf215b1c7968fe7225f7aa64042432fb6e
Florian Boulbes <florian.boulbes@ynov.com>
2022-10-07 23:04:41 +0200

    [UPDATE] add atoi function & add argument for txt

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"math/rand"
+	"os"
 	"strings"
 )
 
@@ -12,12 +13,12 @@ func Random(nbr int) int {
 }
 func game() {
 	input := ""
-	word := readFile("words/words.txt")
+	args := os.Args[1]
+	word := readFile(args)
 	fmt.Println(word)
 	str := string(word[len(word)/2-1])
 	fmt.Println("Good luck, you have 10 attempts")
 	fmt.Print("\nChoose : ")
-	fmt.Scanf("%s", &input)
 	for _, el := range word { // faire une boucle tant que ce n'est pas le mot demandé
 		strToInt := string(el)
 		if strToInt == str {
@@ -38,6 +39,27 @@ func readFile(fileAccess string) string {
 	random := Random(len(split))
 	word := split[random]
 	return word
+}
+
+func Atoi(s string) int {
+	rune := []rune(s)
+	stringToInt := 0
+	for i := 0; i < len(rune); i++ {
+		if s[i] >= '0' && s[i] <= '9' {
+			stringToInt = stringToInt * 10
+			stringToInt = stringToInt + int(rune[i]) - 48
+		}
+		if s[i] == ' ' {
+			return 0
+		}
+	}
+	if s[0] == '-' {
+		stringToInt = stringToInt * -1
+	}
+	if s[0] == '+' && s[1] == '+' || s[0] == '-' && s[1] == '-' {
+		return 0
+	}
+	return stringToInt
 }
 
 func main() {
