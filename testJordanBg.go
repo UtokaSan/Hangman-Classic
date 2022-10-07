9a2833ef9b1566fd7ee7656520a72b0ff4265369
Jordan20031 <79769661+Jordan20031@users.noreply.github.com>
2022-10-07 17:19:10 +0200

    New

--- testJordanBg.go
+++ testJordanBg.go
@@ -1,1 +1,41 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+
+	f, err := os.Open("words/hangman.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	reader := bufio.NewReader(f)
+	buf := make([]byte, 1)
+	fmt.Print(buf)
+
+	for {
+		n, err := reader.Read(buf)
+		if err != nil {
+
+			if err != io.EOF {
+
+				log.Fatal(err)
+			}
+
+			break
+		}
+
+		fmt.Print(string(buf[0:n]))
+	}
+
+	fmt.Println()
+}
